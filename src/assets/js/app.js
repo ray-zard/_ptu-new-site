84ba72d871ef7184730f951d2e933c4f9b4a0cae
ray-zard <ray-zard@mail.ru>
2017-12-08 12:33:47 +0400

    заливаю на сервер

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -105,4 +105,21 @@ $(document).ready(function() {
     arrows: true,
     prevArrow: $(".slick-prev")
   });
+  
+  //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 });
